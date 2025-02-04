fa739e0b306231ae4adda2c3d7c11dfed89de3d2
nk837 <nk9617522@gmail.com>
2025-02-04 16:40:52 +0530

    region updated

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "terraform-state-bucket"
     key            = "terraform/github-actions/terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "us-east-1"
     encrypt        = true
   }
 }
